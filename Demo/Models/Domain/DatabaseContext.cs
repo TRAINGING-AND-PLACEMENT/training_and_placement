79f283361d609e30330ab45dd3394009fdd643d3
Suryakant <suryakantanayak.mca22@scet.ac.in>
2023-03-15 23:17:54 +0530

    Add project files.

---
+++ Demo/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Models.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+    }
+}
