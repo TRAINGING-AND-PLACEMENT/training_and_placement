8bfda32d5b3ddada26748edc639df53e6bd85a56
uday-Singh21 <ud4ysingh@gmail.com>
2023-03-27 22:29:28 +0530

    added sidebar and made pages for company

---
+++ Demo/Controllers/CompanyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TnP.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AppliedCompanies()
+        {
+            return View();
+        }
+        public IActionResult SelectedCompanies()
+        {
+            return View();
+        }
+        public IActionResult CompanyDetails()
+        {
+            return View();
+        }
+    }
+}
