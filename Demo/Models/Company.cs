4967771ad933d688423825c645fbe07fb473e26f
Aniket <santraaniket845@gmail.com>
2023-03-30 15:07:27 +0530

    nothing

---
+++ Demo/Models/Company.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo.Models
+{
+    public class Company
+    {
+        
+        public int id { get; set; }
+        
+        public string cif { get; set; }
+
+        public string name { get; set; }
+
+        public string about { get; set; }
+
+        public string contact { get; set; }
+
+        public string alt_contact { get; set; }
+
+        public string email { get; set; }
+
+        public string alt_email { get; set; }
+
+        public int status { get; set; }
+
+        public string remarks { get; set; }
+
+        public string created_at { get; set; }
+
+        public string updated_at { get; set;}
+    }
+}
