f00bcb6b6d7744982af37188b5ebb1673da5ca1d
Aniket <santraaniket845@gmail.com>
2023-03-30 23:36:05 +0530

    insert operation on company

--- Demo/Controllers/CompanyController.cs
+++ Demo/Controllers/CompanyController.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo.api;
+using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Demo.Controllers.Json;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
 
 namespace Demo.Controllers
 {
     public class CompanyController : Controller
     {
+        HttpClient client;
+        CompanyJson cj =new CompanyJson();
+        public CompanyController()
+        {
+            Webapi wb = new Webapi();
+            System.Uri baseAddress = wb.api();
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,6 +29,25 @@ namespace Demo.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Company model)
+        {
+            if (ModelState.IsValid)
+            {
+                String data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(client.BaseAddress + "companydetails", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String result = response.Content.ReadAsStringAsync().Result;
+                    Debug.Write(result);
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Demo/Controllers/HiringController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class HiringController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
