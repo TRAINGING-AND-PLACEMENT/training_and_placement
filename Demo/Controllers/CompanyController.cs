cf77d364ac9ee31606b56b9809d0b6f8c04a77b8
Aniket <santraaniket845@gmail.com>
2023-03-30 15:43:54 +0530

    changed company controller name ..

--- Demo/Controllers/CompanyController.cs
+++ Demo/Controllers/CompanyController.cs
@@ -1,19 +1,14 @@
-﻿using Demo.api;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Controllers
 {
     public class CompanyController : Controller
     {
-        HttpClient client;
-        public CompanyController() 
-        {
-            Webapi wb = new Webapi();
-            System.Uri baseAddress = wb.api();
-            client = new HttpClient();
-            client.BaseAddress = baseAddress;
-        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
         {
             return View();
         }
