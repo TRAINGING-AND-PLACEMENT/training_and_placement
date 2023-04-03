c95275e230bd34a2007c8705d65eeca056f750dd
Suryakant <suryakantanayak.mca22@scet.ac.in>
2023-04-04 01:36:53 +0530

    login inprocess..

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -31,6 +31,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Login}/{id?}");
 
 app.Run();
