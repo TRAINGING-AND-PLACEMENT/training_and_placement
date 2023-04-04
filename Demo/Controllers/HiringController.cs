f5fcd686427eac7d8e98d9420dc9243b46d1cfb8
Aniket <santraaniket845@gmail.com>
2023-04-04 18:28:41 +0530

    Company index css changed and scet logo added

--- Demo/Controllers/HiringController.cs
+++ Demo/Controllers/HiringController.cs
@@ -8,5 +8,11 @@ namespace Demo.Controllers
         {
             return View();
         }
+
+        public IActionResult Create() 
+        {
+
+            return View(); 
+        }    
     }
 }
