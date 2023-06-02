b8fc4ca1d72ffbbe70bd3642611ff2bae9320bc0
Suryakant <suryakantanayak162@gmail.com>
2023-06-03 02:06:20 +0530

    edit hiring done....

--- Demo/api/Webapi.cs
+++ Demo/api/Webapi.cs
@@ -3,9 +3,9 @@
     public class Webapi
     {
 
-        /*Uri baseAddress = new Uri("http://127.0.0.1/api/api_modify.php?what=");*/
+        Uri baseAddress = new Uri("http://127.0.0.1/api/api_modify.php?what=");
 
-        Uri baseAddress = new Uri("https://tnpdeveloper.000webhostapp.com/api/api_modify.php?what=");
+        /*Uri baseAddress = new Uri("https://tnpdeveloper.000webhostapp.com/api/api_modify.php?what=");*/
 
         public System.Uri api()
         {
