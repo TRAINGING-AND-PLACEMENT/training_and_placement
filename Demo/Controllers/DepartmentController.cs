337b7dd513b9e240a62213af02b5a5c3e4b95d7b
aniket <santraaniket845@gmail.com>
2023-06-12 12:11:13 +0530

    created new Sector and Department Controller and created view folder Department and Sector.. moved files from uploadcsv view folder and moved sectors, department functions to respective department and sector controller..

---
+++ Demo/Controllers/DepartmentController.cs
@@ -0,0 +1,226 @@
+﻿using Azure;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using Demo.api;
+using Demo.Controllers.Json;
+using Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+//using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+
+
+namespace Demo.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IHttpContextAccessor context;
+        HttpClient client;
+        public DepartmentController(IHttpContextAccessor httpContextAccessor)
+        {
+            Webapi wb = new Webapi();
+            System.Uri baseAddress = wb.api();
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+            context = httpContextAccessor;
+        }
+        public void DestorySession()
+        {
+            context.HttpContext.Session.Remove("role");
+            context.HttpContext.Session.Remove("userid");
+            context.HttpContext.Session.Remove("sessionid");
+            context.HttpContext.Session.Remove("studentid");
+        }
+        public IActionResult UploadDepartment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UploadDepartment(IFormFile CSV_File, [FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            string filename = $"{webHostEnvironment.WebRootPath}\\files\\user_csv\\{CSV_File.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(filename))
+            {
+                CSV_File.CopyTo(fileStream);
+            }
+
+            List<Department> model = new List<Department>();
+            var path = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files\user_csv"}" + "\\" + CSV_File.FileName;
+
+            var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                Delimiter = ",",
+                Encoding = Encoding.UTF8,
+                MissingFieldFound = null
+            };
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var department = csv.GetRecord<Department>();
+                    department.status = 0;
+                    department.remarks = "";
+                    department.created_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    department.updated_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    model.Add(department);
+                }
+                String data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(client.BaseAddress + "set_department", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String data2 = response.Content.ReadAsStringAsync().Result;
+                    Debug.Write(data2);
+                    TempData["success"] = "Department inserted.";
+                    return RedirectToAction("View_Department");
+                }
+            }
+            //TempData["error"] = "choose any role to sign in.";
+            return RedirectToAction("Department");
+        }
+
+        public IActionResult Insert_dept()
+        {
+            return View();
+        }
+
+        //single department insert
+        [HttpPost]
+        public IActionResult Insert_dept(Department model)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    model.status = 0;
+                    model.remarks = "";
+                    model.created_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    model.updated_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    String data = JsonConvert.SerializeObject(model);
+                    Debug.WriteLine(data);
+                    StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PostAsync(client.BaseAddress + "set_department", content).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        String result = response.Content.ReadAsStringAsync().Result;
+                        Debug.Write(result);
+                        TempData["success"] = "Department inserted.";
+                        return RedirectToAction("View_Department");
+                    }
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+        public IActionResult View_Department()
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                List<Department> model = new List<Department>();
+
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + "get_department").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String data = response.Content.ReadAsStringAsync().Result;
+                    Debug.WriteLine(data);
+                    var Departments = JsonDecode.FromJson(data);
+                    foreach (var department in Departments.departments)
+                    {
+                        model.Add(department);
+                    }
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+        public IActionResult Edit_Department(int id)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                Department model = new Department();
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + "get_department&id=" + id).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String data = response.Content.ReadAsStringAsync().Result;
+                    Debug.WriteLine(data);
+                    var department = JsonDecode.FromJson(data);
+                    Debug.WriteLine(department);
+                    model = department.departments[0];
+                    Debug.WriteLine(model);
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Update_Department(Department model)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    String data = JsonConvert.SerializeObject(model);
+                    StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PutAsync(client.BaseAddress + "update_department&id=" + model.id, content).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        String result = response.Content.ReadAsStringAsync().Result;
+                        TempData["success"] = "Department Updated";
+                        return RedirectToAction("View_Department");
+                    }
+                    return View();
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+
+
+        public IActionResult Delete_Department(int id)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + "delete_department&id=" + id).Result;
+                return RedirectToAction("View_Department");
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+    }
+}
---
+++ Demo/Controllers/SectorController.cs
@@ -0,0 +1,226 @@
+﻿using Azure;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using Demo.api;
+using Demo.Controllers.Json;
+using Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+//using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+
+namespace Demo.Controllers
+{
+    public class SectorController : Controller
+    {
+        private readonly IHttpContextAccessor context;
+        HttpClient client;
+        public SectorController(IHttpContextAccessor httpContextAccessor)
+        {
+            Webapi wb = new Webapi();
+            System.Uri baseAddress = wb.api();
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+            context = httpContextAccessor;
+        }
+        public void DestorySession()
+        {
+            context.HttpContext.Session.Remove("role");
+            context.HttpContext.Session.Remove("userid");
+            context.HttpContext.Session.Remove("sessionid");
+            context.HttpContext.Session.Remove("studentid");
+        }
+        public IActionResult UploadSector()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UploadSector(IFormFile CSV_File, [FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            string filename = $"{webHostEnvironment.WebRootPath}\\files\\user_csv\\{CSV_File.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(filename))
+            {
+                CSV_File.CopyTo(fileStream);
+            }
+
+            List<Sector> model = new List<Sector>();
+            var path = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files\user_csv"}" + "\\" + CSV_File.FileName;
+
+            var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                Delimiter = ",",
+                Encoding = Encoding.UTF8,
+                MissingFieldFound = null
+            };
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var sector = csv.GetRecord<Sector>();
+                    sector.status = 0;
+                    sector.remarks = "";
+                    sector.created_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    sector.updated_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    model.Add(sector);
+                }
+                String data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(client.BaseAddress + "set_sector", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String data2 = response.Content.ReadAsStringAsync().Result;
+                    Debug.Write(data2);
+                    TempData["success"] = "Sector inserted.";
+                    return RedirectToAction("View_Sector");
+                }
+            }
+
+            return RedirectToAction("Sector");
+        }
+        public IActionResult Insert_sect()
+        {
+            return View();
+        }
+
+        //single sector insert
+        [HttpPost]
+        public IActionResult Insert_sect(Sector model)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    model.status = 0;
+                    model.remarks = "";
+                    model.created_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    model.updated_at = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+                    String data = JsonConvert.SerializeObject(model);
+                    Debug.WriteLine(data);
+                    StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PostAsync(client.BaseAddress + "set_sector", content).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        String result = response.Content.ReadAsStringAsync().Result;
+                        Debug.Write(result);
+                        TempData["success"] = "Sector inserted.";
+                        return RedirectToAction("View_Sector");
+                    }
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+        
+        public IActionResult Edit_Sector(int id)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                Sector model = new Sector();
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + "get_sector&id=" + id).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String data = response.Content.ReadAsStringAsync().Result;
+                    Debug.WriteLine(data);
+                    var sector = JsonDecode.FromJson(data);
+                    Debug.WriteLine(sector);
+                    model = sector.sectors[0];
+                    Debug.WriteLine(model);
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Update_Sector(Sector model)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    String data = JsonConvert.SerializeObject(model);
+                    StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PutAsync(client.BaseAddress + "update_sector&id=" + model.id, content).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        String result = response.Content.ReadAsStringAsync().Result;
+                        TempData["success"] = "Sector Updated.";
+                        return RedirectToAction("View_Sector");
+                    }
+                    return View();
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+        public IActionResult View_Sector()
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                List<Sector> model = new List<Sector>();
+
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + "get_sector").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    String data = response.Content.ReadAsStringAsync().Result;
+                    Debug.WriteLine(data);
+                    var Sectors = JsonDecode.FromJson(data);
+                    foreach (var sector in Sectors.sectors)
+                    {
+                        model.Add(sector);
+                    }
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+            return View();
+        }
+
+        public IActionResult Delete_Sector(int id)
+        {
+            if (@context.HttpContext.Session.GetInt32("role") == 2)
+            {
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + "delete_sector&id=" + id).Result;
+                TempData["success"] = "Sector Deleted.";
+                return RedirectToAction("View_Sector");
+            }
+            else
+            {
+                TempData["serror"] = "You have to login with co-ordinator id and password to access the page.";
+                DestorySession();
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+    }
+}
